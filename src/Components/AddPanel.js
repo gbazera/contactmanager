065531c8e0dce78e83ee9c4df6446252e496f21c
gbazera <bazerashvili.gio27@gmail.com>
2022-07-12 01:03:37 +0400

    Added "add panel"

--- src/Components/AddPanel.js
+++ src/Components/AddPanel.js
@@ -1,8 +1,15 @@
 function AddPanel(){
     return(
-        <div className="add-panel">
+        <div>
             <div className="overlay"></div>
-            
+            <div className="panel">
+                <div className="top">
+                    <p className="title">Add new contact</p>
+                    <button className="close">X</button><br></br>
+                    <label htmlFor="name-input">Name:</label>
+                    <input type="text" name="name-input"/><br></br>
+                </div>
+            </div>
         </div>
     );
 }
